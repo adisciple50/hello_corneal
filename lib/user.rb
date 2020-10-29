8cb0f004abd63ddba464575cd0a2476681ad3690
Jason Crockett <JesusisLord33333@outlook.com>
2020-10-29 16:46:48 +0000

    added more to user authentication

--- lib/user.rb
+++ lib/user.rb
@@ -2,37 +2,42 @@ require_relative '../app/models/author'
 class User
   attr_accessor :username
   # @password = ''
-  @db = false
-  @facebook = false
-  @twitter = false
+  @user = false
   @profile = false
   @profile_pic = false
 
   def initialize username,password
-    @db = Author.find_by({:username => username})
-    self.authenticate! password
+    @user = Author.find_by({:username => username})
   end
 
   def authenticate! password
-    puts @db.to_s
-    # check login exists
-
-    if @db.username == @username
+    if @user.username
       # check password salt
       # https://github.com/codahale/bcrypt-ruby
-      storedpassword = @db.salt.to_s
+      storedpassword = @user.salt.to_s
       hashedpassword = BCrypt::Password.new storedpassword
       if hashedpassword == password
         # generate and store login token
         token = SecureRandom.base64
-        @db.update({token:token})
+        @user.update({token:token})
         return token
+      else
+        return false
       end
     end
   end
 
+  def db
+    @user
+  end
+
   def logged_in?
-    @db.token != '' # if token is blank, user is logged out
+    @user.token != '' # if token is blank, user is logged out
+  end
+
+  def log_out
+    @user.token = ''
+    @user.save!
   end
 
   def logged_out?
@@ -44,27 +49,13 @@ class User
   end
 
   def update_password password
-    @db.salt = new_salt password
-    @db.save!
-  end
-
-  private
-
-  def load_facebook
-    @profile = Facebook.find @db.facebook.id
-  end
-
-  def load_twitter
-    @profile = Twitter.find @db.twitter.id
-  end
-
-  def load_profile
-    @profile = Profile.find @db.profile.id
+    @user.salt = new_salt password
+    @user.save!
   end
 
   def load_profile_pic mappings={profile:0,facebook:1,twitter:2}
-  #   get profile to load pic from
-    case @db.profile_image_type
+    #   get profile to load pic from
+    case @user.profile_image_type
       when 0 # profile picture is default profile
         self.load_profile
         self.update_profile_pic
@@ -77,7 +68,21 @@ class User
       else
         Exception 'profile picture type not implemented - see load profile pic'
     end
-  #   return picture
+    #   return picture
+  end
+
+  private
+
+  def load_facebook
+    @profile = Facebook.find @user.facebook.id
+  end
+
+  def load_twitter
+    @profile = Twitter.find @user.twitter.id
+  end
+
+  def load_profile
+    @profile = Profile.find @user.profile.id
   end
 
   def update_profile_pic
